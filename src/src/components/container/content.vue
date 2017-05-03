95f09a5f411eb817f0c5284459807d3fcd74129d
qing jing <522584145@qq.com>
2017-05-03 17:25:36 +0800

    aa

--- src/src/components/container/content.vue
+++ src/src/components/container/content.vue
@@ -16,76 +16,149 @@
       <ul class="carousel-bottom-new">
         <li>
           <a href="">
-            <p>的撒打算大叔大声道</p>
-            <p>的撒打算大叔大声道</p>
+            <p>啊啊啊啊</p>
+            <p>啊啊啊啊</p>
           </a>
         </li>
         <li>
           <a href="">
-            <p>的撒打算大叔大声道</p>
-            <p>的撒打算大叔大声道</p>
+            <p>啊啊啊啊</p>
+            <p>啊啊啊啊</p>
           </a>
         </li>
         <li>
           <a href="">
-            <p>的撒打算大叔大声道</p>
-            <p>的撒打算大叔大声道</p>
+            <p>啊啊啊啊</p>
+            <p>啊啊啊啊</p>
           </a>
         </li>
         <li>
           <a href="">
-            <p>的撒打算大叔大声道</p>
-            <p>的撒打算大叔大声道</p>
+            <p>啊啊啊啊</p>
+            <p>啊啊啊啊</p>
           </a>
         </li>
         <li>
           <a href="">
-            <p>的撒打算大叔大声道</p>
-            <p>的撒打算大叔大声道</p>
+            <p>啊啊啊啊</p>
+            <p>啊啊啊啊</p>
           </a>
         </li>
       </ul>
     </div>
-    <div class="gn-movie">
-      <a href="#">国内电影 >></a><hr>
+    <div class="hot-movie">
+      <P class="title-list">热门电影</P><hr>
       <div class="movies-list">
         <ul class="clearflex">
         <li v-for=""><a href="">
-          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23393.jpg" alt="">
-          <span></span>
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23057.jpg" alt="">
+          <span>极限特工3：终极回归</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-2/23060.jpg" alt="">
+          <span>功夫瑜伽</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23261.jpg" alt="">
+          <span>帕特森</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23366.jpg" alt="">
+          <span>一条狗的使命</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23391.jpg" alt="">
+          <span>八月</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23389.jpg" alt="">
+          <span>逃出绝命镇</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23057.jpg" alt="">
+          <span>极限特工3：终极回归</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-2/23060.jpg" alt="">
+          <span>功夫瑜伽</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23261.jpg" alt="">
+          <span>帕特森</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23366.jpg" alt="">
+          <span>一条狗的使命</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23391.jpg" alt="">
+          <span>八月</span>
+        </a></li>
+		<li v-for=""><a href="">
+          <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23389.jpg" alt="">
+          <span>逃出绝命镇</span>
         </a></li>
         </ul>
       </div>
     </div>
-    <div class="gn-movie">
-      <a href="#">欧美电影 >></a><hr>
+    <div class="gx-movie">
+		<div class="clearflex">
+      <p class="title-list">资源更新</p>
+	  <ul class="tab-list clearflex">
+		<li>
+		<a  @mouseover="showtab" alt="1">更新</a>
+		</li>
+		<li>
+		<a @mouseover="showtab" alt="2">动作片</a>
+		</li>
+		<li>
+		<a @mouseover="showtab" alt="3">喜剧片</a>
+		</li>
+		<li>
+		<a href="">爱情片</a>
+		</li><li>
+		<a href="">科幻片</a>
+		</li>
+		<li>
+		<a href="">恐怖片</a>
+		</li>
+		<li>
+		<a href="">剧情片</a>
+		</li>
+		<li>
+		<a href="">战争片</a>
+		</li>
+	  </ul>	
+	  </div>
+	  <hr>
       <div class="movies-list">
-        <ul class="clearflex">
+        <ul class="clearflex" v-show="a.a">
           <li><a href="">
-            <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23393.jpg" alt="">
-            <span>sadasdasdas</span>
+            <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23057.jpg" alt="">
+            <span>11111111111111111</span>
           </a></li>
+          
+        </ul>
+		<ul class="clearflex" v-show="a.b">
           <li><a href="">
-            <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23393.jpg" alt="">
-            <span>sadasdasdas</span>
+            <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23057.jpg" alt="">
+            <span>22222222222222</span>
           </a></li>
+          
+        </ul>
+		<ul class="clearflex" v-show="a.c">
           <li><a href="">
-            <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23393.jpg" alt="">
-            <span>sadasdasdas</span>
+            <img src="http://img.idyjy.com/pic/uploadimg/2017-3/23057.jpg" alt="">
+            <span>22222222222222</span>
           </a></li>
-          <li><a href="">
-            <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23393.jpg" alt="">
-            <span>sadasdasdas</span>
-          </a></li>
-          <li><a href="">
-            <img src="http://img.idyjy.com/pic/uploadimg/2017-4/23393.jpg" alt="">
-            <span>sadasdasdas</span>
-          </a></li>
+          
         </ul>
       </div>
     </div>
     <div class="gn-movie">
-      <a href="#">日韩电影 >></a><hr>
+      <a href="#">日韩电影 >></a>
+	  
+	  <hr>
       <div class="movies-list">
         <ul class="clearflex">
           <li><a href="">
@@ -116,25 +189,60 @@
 
 <script>
   export default {
-
-    props:{
-      dalu:{
-
-        }
-    }
+		data (){
+			return {
+				a:{
+					a:true,
+					b:'',
+					c:'',
+					d:'',
+					e:''
+				}	
+			}
+		},
+		methods:{
+			showtab:function (event){
+				var Oli = document.getElementsByClassName('tab-list')[0].querySelectorAll('a');
+				for(var i =0;i<Oli.length;i++){
+					Oli[i].className = '';
+				}
+				event.target.className = "tab-bg";
+				/*switch(event.target.getAttribute('alt'))
+				{
+					case '1':
+						this.a=true,
+						this.b=false
+						break;
+					case '2':
+						this.b=true,
+						this.a=false
+						break;
+					case '3':
+						this.c=true,
+						this.a=false
+						break;
+				}*/
+				for(i in this.a){
+					i = false 
+				}
+				
+			},
+			hidetab:function (event){
+				event.target.className = "";
+				console.log(222);
+			}
+		}
 
   };
-  (function (){
-
-
-
-
-  })();
+  
 </script>
 
 <style lang="less" rel="stylesheet/less">
-
-
+	//复用样式
+	.tab-bg {
+		background:#065fa9;
+		color:#fff;
+	}
     //轮播图
     .content{
       .carousel{
@@ -191,32 +299,77 @@
 
         }
       }
-      .gn-movie{
-        a{
-          display: inline-block;
-          padding: 10px;
-        }
-        .movies-list{
-          padding: 20px 0;
-          li{
-            float: left;
-            margin-left: 50px;
-            &:nth-child(1){
-              margin-left: 0;
-            }
-            a{
-              padding: 0;
-            }
-          }
-          img{
-            width: 160px;
-          }
-        }
-        span{
+      
+	  .hot-movie{
+		padding:10px 5px;
+		
+		.movies-list{
+			a{
+			font-weight:600;
+			color:#333;
+			&:hover{
+			color:red;
+			text-decoration:underline;
+			}
+			}
+		}
+		span{
           font-style: normal;
           display: block;
           text-align: center;
+		  width:145px;
+		  height:30px;
+		  overflow:hidden;
+		  line-height:30px;
         }
-      }
+		li{
+			box-sizing:border-box;
+			width:145px;
+			margin:0 10px;
+			float:left;
+			a{
+			display:block;
+			}
+		}
+		img{
+		height:195px;
+		width:100%;
+		padding:3px;
+		border:1px solid #333;
+		&:hover{
+			border:1px solid red;
+		}
+		}
+		ul{
+			padding:10px 0;
+		}
+	  }
+	  .title-list{
+			padding:5px;
+			display:inline-block;
+			background:#065fa9;
+			color:#fff;
+			
+		}
+		
+		.gx-movie{
+			.title-list{
+				float:left;
+			}
+			.tab-list{
+				float:left;
+				margin-left:200px;
+				li{
+					padding:5px 0;
+					float:left;
+					a{
+						padding:5px 20px;
+						
+					}
+				}
+				
+			}
+        
+		}
     }
 </style>
